b6ee77b6b697b85b2dc3cf784799dcb6b2fc2058
Edward Ross <eross@skeptric.com>
2023-04-07 04:58:45 +0000

    Move data loading to end of train file

--- train_biencoder.py
+++ train_biencoder.py
@@ -14,7 +14,7 @@ python train_bi-encoder-v3.py
 """
 import json
 from torch.utils.data import DataLoader
-from sentence_transformers import SentenceTransformer, LoggingHandler, util, models, losses, InputExample
+from sentence_transformers import SentenceTransformer, LoggingHandler, util, models, losses, InputExample # type: ignore
 import logging
 from datetime import datetime
 import gzip
@@ -32,55 +32,6 @@ QueryId = int
 PassageId = int
 HardNegative = TypedDict('HardNegative', {'qid': QueryId, 'pos': List[PassageId], 'neg': Dict[str, List[PassageId]]})
 TrainQuery = TypedDict('TrainQuery', {'qid': QueryId, 'query': str, 'pos': List[PassageId], 'neg': List[PassageId]})
-
-
-#### Just some code to print debug information to stdout
-logging.basicConfig(format='%(asctime)s - %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    level=logging.INFO,
-                    handlers=[LoggingHandler()])
-#### /print debug information to stdout
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--train_batch_size", default=64, type=int)
-parser.add_argument("--max_seq_length", default=300, type=int)
-parser.add_argument("--model_name", required=True)
-parser.add_argument("--max_passages", default=0, type=int)
-parser.add_argument("--epochs", default=10, type=int)
-parser.add_argument("--pooling", default="mean")
-parser.add_argument("--negs_to_use", default=None, help="From which systems should negatives be used? Multiple systems seperated by comma. None = all")
-parser.add_argument("--warmup_steps", default=1000, type=int)
-parser.add_argument("--lr", default=2e-5, type=float)
-parser.add_argument("--num_negs_per_system", default=5, type=int)
-parser.add_argument("--use_pre_trained_model", default=False, action="store_true")
-parser.add_argument("--use_all_queries", default=False, action="store_true")
-parser.add_argument("--ce_score_margin", default=3.0, type=float)
-args = parser.parse_args()
-
-print(args)
-
-# The  model we want to fine-tune
-model_name = args.model_name
-
-
-max_seq_length = args.max_seq_length            #Max length for passages. Increasing it, requires more GPU memory
-ce_score_margin = args.ce_score_margin             #Margin for the CrossEncoder score between negative and positive passages
-num_negs_per_system = args.num_negs_per_system         # We used different systems to mine hard negatives. Number of hard negatives to add from each system
-num_epochs = args.epochs                 # Number of epochs we want to train
-
-# Load our embedding model
-if args.use_pre_trained_model:
-    logging.info("use pretrained SBERT model")
-    model = SentenceTransformer(model_name)
-    model.max_seq_length = max_seq_length
-else:
-    logging.info("Create new SBERT model")
-    word_embedding_model = models.Transformer(model_name, max_seq_length=max_seq_length)
-    pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(), args.pooling)
-    model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
-
-model_save_path = 'output/train_bi-encoder-mnrl-{}-margin_{:.1f}-{}'.format(model_name.replace("/", "-"), ce_score_margin, datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
 
 
 ### Now we read the MS Marco dataset
@@ -108,10 +59,11 @@ def read_int_value_tsv(filepath: Union[Path, str]) -> Dict[int, str]:
     return data
 
 def get_train_queries(hard_negatives: Iterable[HardNegative],
-                      ce_scores: Dict[QueryId, Dict[int, float]],
+                      ce_scores: Dict[QueryId, Dict[PassageId, float]],
                       ce_score_margin: float,
                       queries: Dict[QueryId, str],
                       negs_to_use: Optional[List[str]] = None,
+                      num_negs_per_system: int = 5,
                       use_all_queries: bool = False) -> Dict[QueryId, TrainQuery]:
     train_queries: Dict[QueryId, TrainQuery] = {}
     for data in hard_negatives:
@@ -150,7 +102,7 @@ def get_train_queries(hard_negatives: Iterable[HardNegative],
             train_queries[qid] = {'qid': qid, 'query': queries[qid], 'pos': pos_pids, 'neg': list(neg_pids)}
     return train_queries
 
-def get_corpus(data_folder=data_folder,
+def load_corpus(data_folder=data_folder,
                collection_url=MSMARCO_COLLECTION_URL) -> Dict[PassageId, str]:
     """Read the corpus files, that contain all the passages.
     
@@ -161,7 +113,6 @@ def get_corpus(data_folder=data_folder,
         download_and_unpack(collection_url, tar_filepath)
     logging.info("Read corpus: collection.tsv")
     return read_int_value_tsv(collection_filepath)
-corpus = get_corpus()
 
 
 def get_queries(data_folder=data_folder,
@@ -172,39 +123,46 @@ def get_queries(data_folder=data_folder,
         tar_filepath = os.path.join(data_folder, 'queries.tar.gz')
         download_and_unpack(queries_url, tar_filepath)
     return read_int_value_tsv(queries_filepath)
-queries = get_queries()
 
-# Load a dict (qid, pid) -> ce_score that maps query-ids (qid) and paragraph-ids (pid)
-# to the CrossEncoder score computed by the cross-encoder/ms-marco-MiniLM-L-6-v2 model
-ce_scores_file = os.path.join(data_folder, 'cross-encoder-ms-marco-MiniLM-L-6-v2-scores.pkl.gz')
-if not os.path.exists(ce_scores_file):
-    logging.info("Download cross-encoder scores file")
-    util.http_get('https://huggingface.co/datasets/sentence-transformers/msmarco-hard-negatives/resolve/main/cross-encoder-ms-marco-MiniLM-L-6-v2-scores.pkl.gz', ce_scores_file)
+def get_ce_scores(data_folder=data_folder) -> Dict[QueryId, Dict[PassageId, float]]:
+    """Load cross encoder scores.
+    
+    Load a dict (qid, pid) -> ce_score that maps query-ids (qid) and paragraph-ids (pid)
+    to the CrossEncoder score computed by the cross-encoder/ms-marco-MiniLM-L-6-v2 model
+    """
+    ce_scores_file = os.path.join(data_folder, 'cross-encoder-ms-marco-MiniLM-L-6-v2-scores.pkl.gz')
+    if not os.path.exists(ce_scores_file):
+        logging.info("Download cross-encoder scores file")
+        util.http_get('https://huggingface.co/datasets/sentence-transformers/msmarco-hard-negatives/resolve/main/cross-encoder-ms-marco-MiniLM-L-6-v2-scores.pkl.gz', ce_scores_file)
+    logging.info("Load CrossEncoder scores dict")
+    with gzip.open(ce_scores_file, 'rb') as fIn:
+        ce_scores = pickle.load(fIn)
+    return ce_scores
 
-logging.info("Load CrossEncoder scores dict")
-with gzip.open(ce_scores_file, 'rb') as fIn:
-    ce_scores = pickle.load(fIn)
+def load_hard_negatives(data_folder=data_folder) -> Iterable[HardNegative]:
+    """Load hard negatives file.
+    
+    As training data we use hard-negatives that have been mined using various systems"""
+    hard_negatives_filepath = os.path.join(data_folder, 'msmarco-hard-negatives.jsonl.gz')
+    if not os.path.exists(hard_negatives_filepath):
+        logging.info("Download cross-encoder scores file")
+        util.http_get('https://huggingface.co/datasets/sentence-transformers/msmarco-hard-negatives/resolve/main/msmarco-hard-negatives.jsonl.gz', hard_negatives_filepath)
 
-# As training data we use hard-negatives that have been mined using various systems
-hard_negatives_filepath = os.path.join(data_folder, 'msmarco-hard-negatives.jsonl.gz')
-if not os.path.exists(hard_negatives_filepath):
-    logging.info("Download cross-encoder scores file")
-    util.http_get('https://huggingface.co/datasets/sentence-transformers/msmarco-hard-negatives/resolve/main/msmarco-hard-negatives.jsonl.gz', hard_negatives_filepath)
+    logging.info("Read hard negatives train file")
+    with gzip.open(hard_negatives_filepath, 'rt') as fIn:
+        for line in tqdm.tqdm(fIn):
+            yield json.loads(line)
 
-
-if args.negs_to_use is not None:
-    negs_to_use = args.negs_to_use.split(",")
-else:
-    negs_to_use = None
-
-logging.info("Read hard negatives train file")
-with gzip.open(hard_negatives_filepath, 'rt') as fIn:
-    hard_negatives = map(json.loads, tqdm.tqdm(fIn))
-    train_queries = get_train_queries(hard_negatives, ce_scores, ce_score_margin, queries, negs_to_use, use_all_queries=args.use_all_queries)
-del ce_scores
-
-logging.info("Train queries: {}".format(len(train_queries)))
-
+def load_train_queries(data_folder=data_folder,
+                       num_negs_per_system=5,
+                       ce_score_margin=0.1,
+                       negs_to_use=None,
+                       use_all_queries=False) -> Dict[QueryId, TrainQuery]:
+    queries = get_queries(data_folder)
+    ce_scores = get_ce_scores(data_folder)
+    hard_negatives = load_hard_negatives(data_folder)
+    train_queries = get_train_queries(hard_negatives, ce_scores, ce_score_margin, queries, negs_to_use, num_negs_per_system=num_negs_per_system, use_all_queries=use_all_queries)
+    return train_queries
 
 # We create a custom MSMARCO dataset that returns triplets (query, positive, negative)
 # on-the-fly based on the information from the mined-hard-negatives jsonl file.
@@ -235,6 +193,63 @@ class MSMARCODataset(Dataset):
 
     def __len__(self):
         return len(self.queries)
+
+
+#### Just some code to print debug information to stdout
+logging.basicConfig(format='%(asctime)s - %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[LoggingHandler()])
+#### /print debug information to stdout
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--train_batch_size", default=64, type=int)
+parser.add_argument("--max_seq_length", default=300, type=int)
+parser.add_argument("--model_name", required=True)
+parser.add_argument("--max_passages", default=0, type=int)
+parser.add_argument("--epochs", default=10, type=int)
+parser.add_argument("--pooling", default="mean")
+parser.add_argument("--negs_to_use", default=None, help="From which systems should negatives be used? Multiple systems seperated by comma. None = all")
+parser.add_argument("--warmup_steps", default=1000, type=int)
+parser.add_argument("--lr", default=2e-5, type=float)
+parser.add_argument("--num_negs_per_system", default=5, type=int)
+parser.add_argument("--use_pre_trained_model", default=False, action="store_true")
+parser.add_argument("--use_all_queries", default=False, action="store_true")
+parser.add_argument("--ce_score_margin", default=3.0, type=float)
+args = parser.parse_args()
+
+print(args)
+
+# The  model we want to fine-tune
+model_name = args.model_name
+
+
+max_seq_length = args.max_seq_length            #Max length for passages. Increasing it, requires more GPU memory
+ce_score_margin = args.ce_score_margin             #Margin for the CrossEncoder score between negative and positive passages
+num_negs_per_system = args.num_negs_per_system         # We used different systems to mine hard negatives. Number of hard negatives to add from each system
+num_epochs = args.epochs                 # Number of epochs we want to train
+negs_to_use = args.negs_to_use.split(",") if args.negs_to_use is not None else None
+
+
+# Load our embedding model
+if args.use_pre_trained_model:
+    logging.info("use pretrained SBERT model")
+    model = SentenceTransformer(model_name)
+    model.max_seq_length = max_seq_length
+else:
+    logging.info("Create new SBERT model")
+    word_embedding_model = models.Transformer(model_name, max_seq_length=max_seq_length)
+    pooling_model = models.Pooling(word_embedding_model.get_word_embedding_dimension(), args.pooling)
+    model = SentenceTransformer(modules=[word_embedding_model, pooling_model])
+
+model_save_path = 'output/train_bi-encoder-mnrl-{}-margin_{:.1f}-{}'.format(model_name.replace("/", "-"), ce_score_margin, datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+
+
+corpus = load_corpus(data_folder=data_folder)
+train_queries = load_train_queries(data_folder=data_folder)
+logging.info("Train queries: {}".format(len(train_queries)))
+
 
 # For training the SentenceTransformer model, we need a dataset, a dataloader, and a loss used for training.
 train_dataset = MSMARCODataset(train_queries, corpus=corpus)
